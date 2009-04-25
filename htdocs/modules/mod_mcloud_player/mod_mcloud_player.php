1bd1f1da6f46d7c716526c0452ca8ce01e7acf5e
metux MediaCloud <weigelt@metux.de>
2009-04-25 23:10:24 +0200

    mod_mcloud_player: added simple video player module

---
+++ htdocs/modules/mod_mcloud_player/mod_mcloud_player.php
@@ -0,0 +1,6 @@
+<?php
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+
+require_once('modules/mod_mcloud_player/player.php');
+$mod_mcloud_player = new mod_mcloud_player($params);
