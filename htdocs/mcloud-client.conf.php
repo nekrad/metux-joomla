f66074627dd6785a782eeb0aedfb53b760d2ccc9
MyLSFM Webmaster <weigelt@metux.de>
2009-05-16 22:09:57 +0200

    MCloud: local config

---
+++ htdocs/mcloud-client.conf.php
@@ -0,0 +1,7 @@
+<?php
+
+define ('MEDIACLOUD_DOWNLOAD_URL',      'http://mcloud.metux.de/bot/download.php?score={SCORE}');
+define ('MEDIACLOUD_SRV_PREFIX',        'http://mcloud.metux.de/bot/');
+define ('MEDIACLOUD_SRV_SECRET',        'wurstbrot999');
+define ('MEDIACLOUD_SRV_NAMESPACE',     'mylsfm');
+define ('MEDIACLOUD_CLASSES',           'libraries/mcloud');
