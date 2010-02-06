5d3aea037ada49f27ce11def08348bbb27bb008c
metux Joomla Development <weigelt@metux.de>
2010-02-06 18:33:01 +0100

    lang.de-ar-DZ: Arabic translation 1.5.10

---
+++ htdocs/language/ar-DZ/ar-DZ.ignore.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* @version		$Id: ar-DZ.ignore.php 10381 2008-09-20  dzconstantine $
+* @package		Joomla! Arabic Algeria Translation
+* @copyright	Copyright (C) 2009 Saber Bousba. All rights reserved. 
+* @copyright	Copyright (C) Translation 2007- 2009 Saber Bousba.
+* @license		GNU/GPL
+* Joomla! is free software. This version may have been modified pursuant
+* to the GNU General Public License, and as distributed it includes or
+* is derivative of works licensed under the GNU General Public License or
+* other free or open source software licenses.
+*/
+
+$search_ignore[] = "و";
+$search_ignore[] = "في";
+$search_ignore[] = "على";
+?>
